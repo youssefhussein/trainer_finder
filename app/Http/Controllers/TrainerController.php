2a4e3506c05e3eaff9c6fd50161dfa6ee74012c9
youssef hussein <20634930+youssefhussein@users.noreply.github.com>
2024-12-21 01:48:48 +0200

    created trainercontroller

---
+++ app/Http/Controllers/TrainerController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Trainer;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class TrainerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index() :View
+    {
+        $trainers_Accepted = Trainer::all();
+        return view('trainers.index', compact('trainers_Accepted'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Trainer $trainer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Trainer $trainer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Trainer $trainer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Trainer $trainer)
+    {
+        //
+    }
+}
